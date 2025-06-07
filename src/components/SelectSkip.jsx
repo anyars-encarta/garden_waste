19ba242a3de8aeb2b5d0a975748ea1a3aee447ab
Anyars Yussif <anyarsencarta@gmail.com>
2025-06-07 12:12:51 +0000

    Correct the min-width of SkipCard to 300px

--- src/components/SelectSkip.jsx
+++ src/components/SelectSkip.jsx
@@ -2,7 +2,7 @@ import SkipCard from "./cards/SkipCard";
 
 const SelectSkip = () => {
   return (
-    <div className="h-screen min-w-[65%] overflow-x-hidden flex flex-col items-center py-10 px-90">
+    <div className="h-screen w-[70%] md:w-[92%] lg:w-[84%] xl:w-[70%] overflow-x-hidden flex flex-col items-center py-10 px-8">
       <h1 className="text-xl font-bold text-blue-600 bg-gray-800 py-1 px-4 rounded-full text-center min-w-[320px]">
         Choose Your Perfect Skip Size
       </h1>
@@ -12,7 +12,7 @@ const SelectSkip = () => {
         restrictions at a glance.
       </span>
 
-      <div className="mt-10 flex justify-between gap-4 flex-wrap">
+      <div className="mt-10 flex justify-between gap-4 flex-wrap w-full">
         <SkipCard />
         <SkipCard />
         <SkipCard />
--- src/components/cards/SkipCard.jsx
+++ src/components/cards/SkipCard.jsx
@@ -1,6 +1,6 @@
 const SkipCard = () => {
   return (
-    <div className="flex-1 border border-gray-500 p-4 rounded-lg bg-gray-900 min-w-[360px] hover:-translate-y-2 transition-transform duration-300 ease-in-out">
+    <div className="flex-1 border border-gray-500 p-4 rounded-lg bg-gray-900 min-w-[300px] hover:-translate-y-2 transition-transform duration-300 ease-in-out">
       SkipCard
     </div>
   );
