5bb72cd860193c45fa9c99f094d53887d100cd6d
Anyars Yussif <anyarsencarta@gmail.com>
2025-06-08 19:35:54 +0000

    Correct width of Selection Details

--- src/components/SelectionDetail.jsx
+++ src/components/SelectionDetail.jsx
@@ -7,7 +7,7 @@ const SelectionDetail = ({ skip }) => {
   const { size, hire_period_days, price_before_vat, vat } = skip;
   return (
     <div className="sticky bottom-0 left-0 w-full flex flex-col">
-      <section className="glassmorphism-gold flex h-[112px] w-full max-w-screen-lg mx-auto items-center justify-between gap-4 sm:gap-8 px-2 sm:px-4 md:px-12">
+      <section className="glassmorphism-gold flex h-[112px] w-full mx-auto items-center justify-between gap-4 sm:gap-8 px-2 sm:px-4 md:px-12">
         <button
           className="bg-blue-600 hover:bg-blue-700 rounded-md text-white px-4 py-2 cursor-pointer"
           onClick={() => {
