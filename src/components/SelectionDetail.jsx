fdf69712ec2f0426c0ef2c7701a3dc9a5b339c04
Anyars Yussif <anyarsencarta@gmail.com>
2025-06-08 19:29:18 +0000

    Correct responsive issues in SelectionDetail component

--- src/components/SelectionDetail.jsx
+++ src/components/SelectionDetail.jsx
@@ -1,15 +1,15 @@
 import { useContext } from "react";
 import SkipContext from "../../context/SkipContext";
 
-const SelectionDetail = (skip) => {
+const SelectionDetail = ({ skip }) => {
   const { setSelectSkipComplete } = useContext(SkipContext);
 
-  const { size, hire_period_days, price_before_vat, vat } = skip.skip;
+  const { size, hire_period_days, price_before_vat, vat } = skip;
   return (
-    <div className="sticky bottom-0 left-0 flex size-full flex-col">
-      <section className="glassmorphism-gold flex h-[112px] w-full items-center justify-between gap-8 px-4 py-4 max-md:justify-center max-md:gap-5 md:px-12">
+    <div className="sticky bottom-0 left-0 w-full flex flex-col">
+      <section className="glassmorphism-gold flex h-[112px] w-full max-w-screen-lg mx-auto items-center justify-between gap-4 sm:gap-8 px-2 sm:px-4 md:px-12">
         <button
-          className="bg-blue-600 hover:bg-blue-700 not-only:rounded-md text-white px-4 py-2 cursor-pointer"
+          className="bg-blue-600 hover:bg-blue-700 rounded-md text-white px-4 py-2 cursor-pointer"
           onClick={() => {
             // Functionality to navigate to previous page
           }}
@@ -17,16 +17,16 @@ const SelectionDetail = (skip) => {
           Back
         </button>
 
-        <div className="flex items-center justify-between max-md:text-center">
-          <p className="w-[50%] text-xs md:text-sm sm:line-clamp-3 text-center max-md:w-3/4 text-gray-100 border-r-2 pr-8">
+        <div className="flex flex-1 items-center justify-between max-md:flex-col max-md:gap-2 max-md:text-center">
+          <p className="hidden lg:block w-1/2 text-xs md:text-sm line-clamp-2 text-center text-gray-100 border-r-2 pr-4">
             Imagery and information shown throughout this website may not
             reflect the exact shape or size specification, colours may vary,
             options and/or accessories may be featured at additional cost.
           </p>
 
-          <div className="flex items-center justify-between w-[50%] pl-4 max-md:w-3/4">
-            <div>
-              <h2 className="text-3xl sm:text-lg text-white">
+          <div className="flex items-center justify-between w-full lg:w-1/2 pl-0 lg:pl-4 max-md:w-full">
+            <div className="text-left max-md:text-center">
+              <h2 className="text-lg md:text-3xl text-white">
                 {size} Yard Skip
               </h2>
               <p className="text-md text-gray-500">
@@ -34,8 +34,8 @@ const SelectionDetail = (skip) => {
               </p>
             </div>
 
-            <div classname="flex flex-col items-end">
-              <h2 className="text-3xl sm:text-lg text-blue-600 font-bold">
+            <div className="flex flex-col items-end max-md:items-center ml-4">
+              <h2 className="text-2xl md:text-3xl text-blue-600 font-bold">
                 £
                 {(price_before_vat + vat || 0).toLocaleString("en-GB", {
                   minimumFractionDigits: 2,
@@ -49,10 +49,7 @@ const SelectionDetail = (skip) => {
 
         <button
           className="bg-blue-600 hover:bg-blue-700 rounded-md text-white px-4 py-2 cursor-pointer"
-          onClick={
-            () => setSelectSkipComplete(true)
-            // App then navigates to the next page
-          }
+          onClick={() => setSelectSkipComplete(true)}
         >
           Next
         </button>
